b206365d582f63af415a79af8e710123ad981373
Velda Kiara <veldakarimi@gmail.com>
2020-03-11 00:13:39 +0300

    Initial commit

---
+++ SimpleArraySum/SimpleArraySum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    result = sum(ar)
+    return result
+        
+
+
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
